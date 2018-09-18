93432ac7aa0922d6a92b240dc9234da9be3ca009
mfbradley <mbradleystylist@gmail.com>
2018-09-17 21:52:59 -0400

    refactoring to dynamically create questions and answers, reorganized as object in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,55 @@
 // START BUTTON AND COUNTDOWN TIMER
+
+/* 
+            
+           
+            
+            
+            <div class="row doneButton">
+                <div class="col-sm-12 col-md-12">
+                    <p id="done" class="btn btn-primary btn-lg" role="button">Done!</p>
+                </div>
+            </div> */
 window.onload = function() {
+ 
+    var qA = [
+        {
+            q: "Which two characters have matching tatoos of the date they landed their GOT roles?",
+            a: ["Ygritte and Jon Snow", "Cersei and Jamie Lannister", "Sansa and Arya Stark", "Daenerys and Tyrion"], 
+            correct: "Sansa and Arya Stark"
+        },
+
+        {
+            q: "Which character was almost cast as Jon Snow?",
+            a: ["Gendry", "Theon Greyjoy", "Podrick Payne", "Ramsay Bolton"], 
+            correct: "Ramsay Bolton"
+
+        },
+
+        {
+            q: "Which Stark adopted his/her dierwolf in real life?", 
+            a: ["Bran", "Sansa", "Arya", "Rickon"],
+            correct: "Sansa"
+        },
+
+        {
+            q: "Which actor got into a fight at a McDonald's the night before his audition, resulting in a black eye that helped him land the role?",
+            a: ["Kit Harrington", "Jason Momoa", "Peter Dinklage", "Jerome Flynn"],
+            correct: "Kit Harrington"
+        },
+        {
+            q: "Which GOT characters used to date in real life?",
+            a: ["Hodor and Gilly", "Jon Snow and Ygritte", "Sansa and Gendry", "Cersei and Bronn"],
+            correct: "Cersei and Bronn"
+        },
+        {
+            q: "Which two characters were besties before the show began filming?",
+            a: ["Tormund and Brienee of Tarth", "Tyrion and Cersei", "Khal Drogo and The Mountain", "Katyln Stark and Cersei"],
+            correct: "Tyrion and Cersei"
+        }
+    ]
+
+    
     $("#wrapper").hide();
     $("#results").hide();
     $("#title").show();
